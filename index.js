aa186233e1e141a155c83f52ae3135b170bc164c
Nicholas Fagner <nicholasfagner@gmail.com>
2021-10-14 16:21:50 -0600

    commit tree 4598d6f2408872528b0e14666ba734bf77d65c2f

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(array, string){
+    const drivers = array.filter(driver => driver.toUpperCase() === string.toUpperCase())
+    return drivers
+}
+
+function fuzzyMatch(array, string){
+    const drivers = array.filter(driver => driver.startsWith(string))
+    return drivers; 
+}
+
+function matchName(array, string){
+    const drivers = array.filter(driver => driver.name === string)
+    return drivers;
+}
